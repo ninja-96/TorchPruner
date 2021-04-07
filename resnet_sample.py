2c4660941e78c26b4e54af22d1b8e3d422d2a641
Oleg Kachalov <43884433+ninja-96@users.noreply.github.com>
2021-04-07 16:56:47 +0300

    Update resnet_sample.py
    fix features

--- resnet_sample.py
+++ resnet_sample.py
@@ -3,6 +3,6 @@ from backbones.resnet import resnet50
 from TorchPruner import prune_model
 
 model = resnet50(pretrained=True).eval()
-proto = resnet50(features=[16, 32, 64, ]).eval()
+proto = resnet50(features=[16, 32, 64, 64]).eval()
 
 pruned_model = prune_model(model, proto)
